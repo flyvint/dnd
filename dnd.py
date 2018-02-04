8be972965fb151b97e9f6ab6c69e2a302aa369bd
vint <flyvint@gmail.com>
2018-02-05 00:20:23 +0300

    move all to class

--- dnd.py
+++ dnd.py
@@ -4,54 +4,19 @@ import requests
 import lxml.html
 import re
 import datetime
+import edutatardataprovider
 
-url_0 = "https://edu.tatar.ru"
+
 
 user="50194001176"
 passwd="vDhCHmfE"
 
-hdr= {}
-hdr["User-Agent"]= "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+dprov= edutatardataprovider.EduTatarDataProvider()
+if dprov.login(user, passwd) == False:
+	print( "login failed" )
+	exit(1)
 
-def get_url( url ):
-    r= s.get( url_0 + url, headers= hdr, verify=True )
-    return lxml.html.document_fromstring( r.text )
-
-
-print("login")
-s = requests.session()
-get_url( "/logon" )
-
-hdr["Referer"]=  "https://edu.tatar.ru/logon"
-logindata= { "main_login":user, "main_password":passwd }
-r = s.post( url_0 + "/logon", data=logindata, headers= hdr, allow_redirects=True, verify=True )
-
-if "Неверный логин" in r.text:
-    print( "login failed" )
-    exit(1)
-    
-if "Личный кабинет" not in r.text:
-    print("something wrong")
-    exit(2)
-
-print("login ok")
-
-hdr["Referer"]=  "https://edu.tatar.ru"
-
- # FIXME
-secsfromepoch=datetime.datetime.now().timestamp()
-print("curdate:", secsfromepoch)
-secsfromepoch=1517259600
-doc= get_url( "/user/diary/day?for=" + str(secsfromepoch) )
-
-# aaa= doc.xpath(".//div[@class='d-table']//tbody/tr/td[2]/text()")
-# print( "a:", aaa)
-
-dairyrowArr= doc.xpath(".//div[@class='d-table']//tbody/tr")
-print( "dairyrowArr:", dairyrowArr)
-
-for dr in dairyrowArr
-
+dprov.get_marks_for_day( datetime.datetime(2018,1,31) )
 
 
 
---
+++ edutatardataprovider.py
@@ -0,0 +1,75 @@
+import requests
+import lxml.html
+import re
+import datetime
+
+class Marks:
+
+	def __init__(self):
+		self.marks= ()
+		
+
+
+class EduTatarDataProvider:
+	
+	def __init__(self):
+		self.hdr= {}
+		self.hdr["User-Agent"]= "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+		self.url_0= "https://edu.tatar.ru"
+		pass
+
+	def get_url( self, url ):
+		self.hdr["Referer"]=  "https://edu.tatar.ru"
+		r= self.s.get( self.url_0 + url, headers= self.hdr, verify=True )
+		return lxml.html.document_fromstring( r.text )
+
+	def login(self, user, passwd):
+		self.user= user
+		self.passwd= passwd
+
+		self.s = requests.session()
+		self.get_url( "/logon" )
+		
+		self.hdr["Referer"]=  "https://edu.tatar.ru/logon"
+		logindata= { "main_login":user, "main_password":passwd }
+		r = self.s.post( self.url_0 + "/logon",
+					data=logindata,
+					headers= self.hdr,
+					allow_redirects=True, verify=True )
+
+		if "Неверный логин" in r.text:
+		    print( "login failed" )
+		    return False
+		    
+		if "Личный кабинет" not in r.text:
+		    print("something wrong")
+		    return False
+
+		print("login ok")
+		return True
+
+	def get_marks_for_day(self, date):
+		ts= int( date.timestamp() )
+		# print("ts:", ts)
+		doc= self.get_url( "/user/diary/day?for=" + str(ts) )
+
+		dairyRowArr= doc.xpath(".//div[@class='d-table']//tbody/tr")
+		print( "dairyRowArr:", dairyRowArr)
+
+		markslist= ()
+		for dr in dairyRowArr:
+			marks= dr.xpath( ".//table[@class='marks']//td" )
+			if len(marks) > 0:
+				subj= dr.xpath( "./td[2]/text()")
+				print("subj:", subj)
+				print("marks:", marks)
+				for m in marks:
+					mreasons= m.xpath("./@title")
+					print("  reasons:", mreasons)
+					mmarks= m.xpath(".//div/text()")
+					print("  marks:", mmarks)
+					markreason= zip( mmarks, mreasons )
+					print("  markreason:", markreason )
+
+
+

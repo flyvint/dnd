b64b24e3821bf4b6848f0e74849c998948168493
vint <flyvint@gmail.com>
2018-02-12 09:02:26 +0300

    + taking auth params from config

--- dnd.py
+++ dnd.py
@@ -5,9 +5,12 @@ import lxml.html
 import re
 import datetime
 import edutatardataprovider
+import configparser
 
-user="50194001176"
-passwd="vDhCHmfE"
+conf= configparser.ConfigParser()
+conf.read( "dnd.conf" )
+user=  conf[ "EduTatarRuAuth" ][ "user" ]
+passwd=conf[ "EduTatarRuAuth" ][ "password" ]
 
 dprov= edutatardataprovider.EduTatarDataProvider()
 if dprov.login(user, passwd) == False:
